1e0a75e678d592df61bf3c1741d225802c08f49c
David Gay <djg9610@gmail.com>
2018-02-15 20:33:32 -0500

    progress

--- src/main.cpp
+++ src/main.cpp
@@ -89,6 +89,7 @@ int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
 }
 
 // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
+// 2/15/18 Commit most recent code
 vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
 {
 	int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
