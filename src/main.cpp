0a4017eea587e4e6cacb0224f0b623c1f3c87c9a
David Gay <djg9610@gmail.com>
2018-02-16 22:54:29 -0500

    Progress on spline function. Better car tracking.
    + Way to follow other cars closer to improve cost function.
    + Progress on spline function.
    - Bug where ego car would slow down too much.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
 #include <string>
+#include "spline.h"
 
 using namespace std;
 
@@ -168,9 +169,14 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 
 }
 
-double mphToSpacing(double mph)
+double speedToSpacing(double speed, bool in_meters)
 {
-  return (mph * 0.44704) / 50;
+  if (in_meters)
+  {
+    speed *= 2.2369;
+  }
+
+  return (speed * 0.44704) / 50;
 }
 
 
@@ -256,6 +262,9 @@ int main()
             double end_path_s = j[1]["end_path_s"];
             double end_path_d = j[1]["end_path_d"];
 
+            // Store size of previous path. (Assumes x size == y size)
+            int size_prev previous_path_x.size();
+
             // Sensor Fusion Data, a list of all other cars on the same side of the road.
             auto sensor_fusion = j[1]["sensor_fusion"];
 
@@ -269,12 +278,15 @@ int main()
 
             // INFO: Keeps car in specified lane at 50 mph with no regard to other cars.
 
-            double dist_max = mphToSpacing(50);
-            double dist_min = mphToSpacing(25);
+            bool debug = true;
+
+
+            double dist_max = speedToSpacing(50, false);
+            double dist_min = speedToSpacing(25, false);
             double static STOP_COST = 0.8;
 
             // Assumes that it's better to navigate to the left. Can be changed.
-            vector<double> costs = {0,0.3,0.3};
+            vector<double> costs = {0,0.15,0.15};
 
             int best_index;
 
@@ -289,7 +301,84 @@ int main()
             double d = 0;
             
 
-            //costs[0] = ((dist_max - current_spacing) / dist_max);
+            // Initialize x and y vectors to be used for spline calculation. 
+            vector <double> spl_x;
+            vector <double> spl_y; 
+
+
+            // Store reference states for car x,y,yaw.
+            double ref_x = car_x;
+            double ref_y = car_y;
+            // Convert to radians
+            double ref_yaw = deg2rad(car_yaw);
+
+
+            // Decide whether to use car or previous path end point as starting reference point.
+
+            // If size of previous is less than 2, use car as starting point.
+            if (size_prev < 2)
+            {
+
+              // Calculate points tangent to car.
+              double prev_x = car_x - cos(car_yaw);
+              double prev_y = car_y - sin(car_yaw);
+
+              // Push back tangent x,y points and current car x,y points onto spline point vector.
+              spl_x.push_back(prev_x);
+              spl_x.push_back(car_x);
+              spl_y.push_back(prev_y);
+              spl_y.push_back(car_y);
+            }
+            // If size greater than 2, use the previous path end point as starting point.
+            else
+            { 
+              // Update reference state to be previous path end point.
+              ref_x = previous_path_x[size_prev-1];
+              ref_y = previous_path_y[size_prev-1];
+
+              // Calculate tangent points.
+              double ref_x_prev = previous_path_x[size_prev-2];
+              double ref_y_prev = previous_path_y[size_prev-2];
+              ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+              // Push back x,y points tangent to previous path end point onto spline point vector.
+              spl_x.push_back(ref_x_prev);
+              spl_x.push_back(ref_x);
+              spl_y.push_back(ref_y_prev);
+              spl_y.push_back(ref_y);
+            }
+
+
+            // Calculate future waypoints from 30m - 90m.
+            vector <double> waypoints0 = getXY(car_s + 30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector <double> waypoints1 = getXY(car_s + 60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector <double> waypoints2 = getXY(car_s + 90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+            // Push back waypoints onto spline vector.
+            spl_x.push_back(waypoints0[0]);
+            spl_x.push_back(waypoints1[0]);
+            spl_x.push_back(waypoints2[0]);
+
+            spl_y.push_back(waypoints0[1]);
+            spl_y.push_back(waypoints1[1]);
+            spl_y.push_back(waypoints2[1]);
+
+
+            // Loop over points in spline vector and shift coordinate reference from global to local (car) coordinates.
+            for (int n = 0; n < spl_x.size(); n++)
+            {
+              double shift_x = spl_x[n] - ref_x;
+              double shift_y = spl_y[n] - ref_y;
+
+              spl_x = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
+              spl_y = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
+            }
+
+            // Initialize spline.
+            tk::spline s;
+
+            // TODO: 30:51: Implement spline and get working with current frame.
+
 
 
             vector<double> lanes_to_check = {0};
@@ -299,21 +388,18 @@ int main()
               lanes_to_check.resize(1);
               lanes_to_check[0] = (1);
               costs[1] = (1);
-              //d = ((2+4) * lane) + 2;
             }
             else if (lane == 1)
             {
               lanes_to_check.resize(2);
               lanes_to_check[0] = (0);
               lanes_to_check[1] = (2);
-              //d = ((2+4) * lane);
             }
             else if (lane == 2)
             {
               lanes_to_check.resize(1);
               lanes_to_check[0] = (1);
               costs[2] = (1);
-              //d = ((2+4) * lane) - 2;
             }
 
             for (int n = 0; n < sensor_fusion.size(); n++)
@@ -328,8 +414,8 @@ int main()
                   {
                     double s_to_check = sensor_fusion[n][5];
 
-                    // Measures if a car is further in front than in rear to prevent 
-                    if (s_to_check > car_s && (s_to_check - car_s) < 20 || s_to_check < car_s && abs((s_to_check - car_s)) < 10)
+                    // Measures if a car is close in other lane. Forward facing and rear distance are set independently. 
+                    if (s_to_check > car_s && (s_to_check - car_s) < 20 || s_to_check < car_s && (car_s - s_to_check) < 10)
                     {
                       if (lanes_to_check.size() == 2)
                       {
@@ -348,7 +434,6 @@ int main()
                 }
             }
 
-
             for (int n = 0; n < sensor_fusion.size(); n++)
             {
             
@@ -356,11 +441,16 @@ int main()
 
               if (d < (2+4*lane+2) && d > (2+4*lane-2))
               {
+                
                 double other_car_s = sensor_fusion[n][5];
 
-                if (other_car_s > car_s && (other_car_s - car_s) < 30)
+                if (other_car_s > car_s && (other_car_s - car_s) <= 15)
                 {
+                  double vx = sensor_fusion[n][3];
+                  double vy = sensor_fusion[n][4];
+                  double other_car_speed = sqrt(vx*vx+vy+vy);
                   speed_up = false;
+                  dist_min = speedToSpacing(other_car_speed, true);
                 }
 
               }
@@ -380,10 +470,23 @@ int main()
 
             best_index = min_pos;
 
-            cout << "Costs:" << costs[0] << " : " << costs[1] << " : " << costs[2] << endl;
+            if (debug)
+            {
+              cout << "Costs: " << costs[0] << " : " << costs[1] << " : " << costs[2] << endl;
 
-            cout << "Best index: " << best_index << endl;
-
+              if (best_index == 0)
+              {
+                cout << "Best Action: Stay in lane." << endl;
+              }
+              else if (best_index == 1)
+              {
+                cout << "Best Action: Change to left." << endl;
+              }
+              else if (best_index == 2)
+              {
+                cout << "Best Action: Change to right." << endl;
+              }
+            }
 
             iteration++;
 
@@ -429,7 +532,7 @@ int main()
               }
               else
               {
-                current_spacing -= (0.0015 / 50);
+                current_spacing -= (0.004 / 50);
 
                 if (current_spacing < dist_min)
                 {
